a56d4de2adec920c229f1438dcc68aa86c0d7855
phathung2001 <phathung2001@gmail.com>
2020-01-16 18:35:48 +0700

    Add Tinhdiem

---
+++ tinhDiem.js
@@ -0,0 +1,84 @@
+﻿var toHop = "";
+var toan, van, anh, th1, th2, th3, tongket, kkhich, uutien, diem, diemToHop;
+var hsBaiThi = 0.5, hsTrongNam = 0.5;
+
+$(window).on("load", function(){
+    $(".container-fluid").addClass("w3-animate-top");
+    $(".container-fluid").attr("style", "display:block;");
+});
+
+$("#btnKHTN").click(function(){
+    // Đổi placeholder
+    $("#th1").attr("placeholder", "Lý");
+    $("#th2").attr("placeholder", "Hoá");
+    $("#th3").attr("placeholder", "Sinh");
+    // Đổi label của dropdown
+    $("#btnToHop").text("KHTN");
+    //Hiện table
+    showTable();
+});
+
+$("#btnKHXH").click(function(){
+    // Đổi placeholder
+    $("#th1").attr("placeholder", "Sử");
+    $("#th2").attr("placeholder", "Địa");
+    $("#th3").attr("placeholder", "GDCD");
+    // Đổi label của dropdown
+    $("#btnToHop").text("KHXH");
+    //Hiện table
+    $showTable();
+});
+
+$("#btnModalClose").click(function(){
+    $("#alertModal").modal("hide");
+});
+
+$("#btnSubmit").click(function(){
+    toan = getValue("#toan");
+    van = getValue("#van");
+    anh = getValue("#anh");
+    th1 = getValue("#th1");
+    th2 = getValue("#th2");
+    th3 = getValue("#th3");
+    tongket = getValue("#tongket");
+    kkhich = getValue("#kkhich");
+    uutien = getValue("#uutien");
+
+    diemToHop = (th1 + th2 + th3) / 3;
+    diem = ((toan + van + anh + diemToHop + kkhich)/4)*hsBaiThi + tongket*hsTrongNam + uutien;
+    diem = diem.toFixed(2);
+    if (diem >= 5){
+        thongBaoKQ("Đậu", diem);
+	}
+    else{
+        thongBaoKQ("Rớt", diem);
+	}
+});
+
+function test(){
+    $("#toan").val(8);
+    $("#van").val(2);
+    $("#anh").val(8.5);
+    $("#th1").val(8);
+    $("#th2").val(2);
+    $("#th3").val(2);
+    $("#tongket").val(8.5);
+    showTable();
+}
+
+function showTable(){
+    $("form").addClass("w3-animate-top");
+    $("form").attr("style", "dislay:block;");
+}
+
+function thongBaoKQ(txt, diem){
+    $("#txtKQ").text(txt);
+    $("#diemKQ").text(diem);
+    $("#alertModal").modal("show");
+}
+
+function getValue(id){
+    var value = parseInt($(id).val());
+    if (isNaN(value)) return 0;
+    return value;
+}
