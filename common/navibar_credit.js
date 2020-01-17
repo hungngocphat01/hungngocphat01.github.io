99b6a4ba0f24e7c29299e22223093a67fa2db773
phathung2001 <phathung2001@gmail.com>
2020-01-17 09:58:26 +0700

    Navibar selector fix

--- common/navibar_credit.js
+++ common/navibar_credit.js
@@ -1,8 +1,8 @@
 var navibar = 
-`<a class="link-not-selected" id="naviHome" href="index.html">About me</a>
-<a class="link-not-selected" id="naviPosts" href="notAvailable.html">My posts</a>
-<a class="link-not-selected" id="naviProducts" href="myProducts.html">My products</a>
-<a class="link-not-selected" id="naviUnavailable" style="display:none;" href="notAvailable.html">Unavailable</a>
+`<a class="link-not-selected" id="naviHome" href="/index.html">About me</a>
+<a class="link-not-selected" id="naviPosts" href="/notAvailable.html">My posts</a>
+<a class="link-not-selected" id="naviProducts" href="/products/myProducts.html">My products</a>
+<a class="link-not-selected" id="naviUnavailable" style="display:none;" href="#">Unavailable</a>
 <hr>`;
 var credit=
 `Copyright (c) 2020 hungngocphat01.<br/>
@@ -12,3 +12,7 @@ $(window).on("load", function(){
     $("#navibar").append(navibar);
     $("#credit").append(credit);
 });
+
+function navibarSelect(id){
+    $(id).attr("class", "link-selected");
+}
