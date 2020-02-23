66d52b898769b5a1a7c0b285c93b242cdf759762
hungngocphat01 <phathung2001@gmail.com>
2020-02-23 18:16:04 +0000

    Big update

--- common/navibar_credit.js
+++ common/navibar_credit.js
@@ -1,8 +1,8 @@
 var navibar = 
-`<a class="link-not-selected" id="naviHome" href="/index.html">About me</a>
-<a class="link-not-selected" id="naviPosts" href="/notAvailable.html">My posts</a>
-<a class="link-not-selected" id="naviProducts" href="/products/myProducts.html">My products</a>
-<a class="link-not-selected" id="naviUnavailable" style="display:none;" href="#">Unavailable</a>
+`<a class="link-not-selected" id="naviHome" href="/index.html">About me </a>
+<a class="link-not-selected" id="naviPosts" href="/posts/myPosts.html">My posts </a>
+<a class="link-not-selected" id="naviProducts" href="/products/myProducts.html">My products </a>
+<a class="link-not-selected" id="naviUnavailable" style="display:none;" href="#">Unavailable </a>
 <hr>`;
 var credit=
 `Copyright (c) 2020 hungngocphat01.<br/>
