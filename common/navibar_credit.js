d575125c0552b9fb6658eb0741cad4d7d8c665ef
phathung2001 <phathung2001@gmail.com>
2020-02-26 11:27:31 +0700

    Chang html file names

--- common/navibar_credit.js
+++ common/navibar_credit.js
@@ -1,7 +1,7 @@
 var navibar = 
-`<a class="link-not-selected" id="naviHome" href="/index.html">About me </a>
-<a class="link-not-selected" id="naviPosts" href="/posts/myPosts.html">My posts </a>
-<a class="link-not-selected" id="naviProducts" href="/products/myProducts.html">My products </a>
+`<a class="link-not-selected" id="naviHome" href="/">About me </a>
+<a class="link-not-selected" id="naviPosts" href="/posts/">My posts </a>
+<a class="link-not-selected" id="naviProducts" href="/products/">My products </a>
 <a class="link-not-selected" id="naviUnavailable" style="display:none;" href="#">Unavailable </a>
 <hr>`;
 var credit=
